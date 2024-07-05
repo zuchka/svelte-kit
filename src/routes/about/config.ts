340327fa751ff7b83d00b2659df27598b515585b
zuchka <>
2024-07-04 18:08:37 -0700

    update redirect

--- src/routes/about/config.ts
+++ src/routes/about/config.ts
@@ -1,8 +1,8 @@
 // export const CLIENT_ID = "2a8aa9f2-ee5a-4f9d-bc7a-e4609d9dd83c" // this works for patient facing app
-export const CLIENT_ID = "625d5274-1c35-4cf9-bcff-738685558eb8" // this is mine for practitionar app
+export const CLIENT_ID = "625d5274-1c35-4cf9-bcff-738685558eb8" // this is mine for practitioner app
 export const FHIR_BASE_URL = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4"
 export const SMART_AUTH_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize"
 export const SMART_TOKEN_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
-export const REDIRECT_URL = 'http://localhost:5173/'
+export const REDIRECT_URL = 'https://em.doctor/about'
 export const CODE_VERIFIER_LOCAL_STORAGE_KEY = 'smart_code_verifier'
 export const TOKEN_RESPONSE_LOCAL_STORAGE_KEY = 'smart_token_response'
